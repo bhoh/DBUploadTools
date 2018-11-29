2635114bd06b65ac0fc1718dd00c41fd15c0e2f1
SangEun Lee <d4space@gmail.com>
2018-11-29 17:50:25 +0100

    up

---
+++ JR/uploadJRConditions.sh
@@ -0,0 +1,8 @@
+#./uploadJRConditions.py --db Summer15_25nsV6_MC.db --era Summer15_25nsV6_MC  for the test
+#./uploadJRConditions.py --db Summer15_25nsV6_MC.db --era Summer15_25nsV6_MC --production to upload
+
+
+
+
+./uploadJRConditions.py --db Fall17_V3_94X_MC.db --era Fall17_V3_94X_MC --production
+#./uploadJRConditions.py --db Fall17_V3_94X_DATA.db --era Fall17_V3_94X_DATA --production
