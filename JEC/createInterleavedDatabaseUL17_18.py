42b51e7227b205d511173fd6163a578da5baf198
ByungHun Oh <bh.oh@cern.ch>
2020-12-11 19:20:44 +0900

    createInterleavedDatabaseUL17_18

---
+++ JEC/createInterleavedDatabaseUL17_18.py
@@ -0,0 +1,183 @@
+#! /usr/bin/env python
+import os
+import re
+import sys
+import subprocess
+import argparse
+
+#########################################################
+# usage: python createInterleavedDatabase16_17.py --era Summer16_03Feb2017All_V9_DATA --importDB Summer16_03Feb2017BCD_V9_DATA.db
+#########################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--era', metavar='F', action='store', dest='era', help='Output era', required=True)
+parser.add_argument('--importDB', nargs='+', metavar='DB', action='store', dest='db_import', help='starting database', required=True)
+#parser.add_argument('--EndDB', metavar='DB', action='store', dest='db_end', help='Ending database', required=True)
+
+options = parser.parse_args()
+'''
+if not os.path.exists(options.db_import):
+    raise argparse.ArgumentTypeError('importing database does not exist: %r' % options.db_import)
+'''
+#if not os.path.exists(options.db_end):
+#    raise argparse.ArgumentTypeError('25ns database does not exist: %r' % options.db_end)
+
+jec_type    = 'JetCorrectorParametersCollection'
+ERA         = options.era
+algsizetype_AK4 = {'ak': [4]} #other options: ic, kt and any cone size
+algsizetype_AK8 = {'ak': [8]} #other options: ic, kt and any cone size
+jettype_base = ['pf', 'pfchs', 'puppi']
+jettype_ext  = ['calo', 'jpt']
+#jettype = ['pf', 'pfchs', 'puppi'] #other options: calo
+
+ALGO_LIST_AK4_base = []
+for k, v in algsizetype_AK4.iteritems():
+    for s in v:
+        for j in jettype_base:
+	    ALGO_LIST_AK4_base.append(str(k.upper()+str(s)+j.upper().replace("CHS","chs").replace("PUPPI","PFPuppi").replace("CALO","Calo")))
+            #ALGO_LIST.append(str(k.upper()+str(s)+j.upper().replace("CHS","chs").replace("PUPPI","PFPuppi")))
+
+ALGO_LIST_AK4_ext = []
+for k, v in algsizetype_AK4.iteritems():
+    for s in v:
+        for j in jettype_ext:
+	    ALGO_LIST_AK4_ext.append(str(k.upper()+str(s)+j.upper().replace("CHS","chs").replace("PUPPI","PFPuppi").replace("CALO","Calo")))
+            #ALGO_LIST.append(str(k.upper()+str(s)+j.upper().replace("CHS","chs").replace("PUPPI","PFPuppi")))
+
+ALGO_LIST_AK8_base = []
+for k, v in algsizetype_AK8.iteritems():
+    for s in v:
+        for j in jettype_base:
+	    ALGO_LIST_AK8_base.append(str(k.upper()+str(s)+j.upper().replace("CHS","chs").replace("PUPPI","PFPuppi").replace("CALO","Calo")))
+            #ALGO_LIST.append(str(k.upper()+str(s)+j.upper().replace("CHS","chs").replace("PUPPI","PFPuppi")))
+
+db_import= options.db_import
+#db_end = options.db_end
+
+iov_list=[]
+for db in db_import:
+    if not os.path.exists(db):
+        raise RuntimeError('%s is not exist'%db)
+    errorFlag = False
+    if re.search(r'^Summer19UL18_', db):
+        if re.search(r'RunA_V[0-9]*', db):
+            iov_list.append(("307083", "316997", db))
+        elif re.search(r'RunB_V[0-9]*', db):
+            iov_list.append(("316998", "319312", db))
+        elif re.search(r'RunC_V[0-9]*', db):
+            iov_list.append(("319313", "320393", db))
+        elif re.search(r'RunD_V[0-9]*', db):
+            iov_list.append(("320394", "", db))
+        else:
+            errorFlag=True
+    elif re.search(r'^Summer19UL17_', db):
+        if re.search(r'RunB_V[0-9]*', db):
+            iov_list.append(("294645", "299329", db))
+        elif re.search(r'RunC_V[0-9]*', db):
+            iov_list.append(("299337", "302029", db))
+        elif re.search(r'RunDE_V[0-9]*', db):
+            iov_list.append(("302030", "304826", db))
+        elif re.search(r'RunD_V[0-9]*', db):
+            iov_list.append(("302030", "303434", db))
+        elif re.search(r'RunE_V[0-9]*', db):
+            iov_list.append(("303435", "304826", db))
+        elif re.search(r'RunF_V[0-9]*', db):
+            iov_list.append(("304911", "307082", db))
+        else:
+            errorFlag=True
+    else:
+        errorFlag=True
+
+    if errorFlag:
+        raise RuntimeError('an error occurred during the setting run range of %s'%db)
+
+##iov_list = [
+##	("1", "252999",      db_start),
+##	("253000", "254832", db_end),
+##	("254833", "254833", db_start),
+##	("254834", "254980", db_end),
+##	("254981", "255031", db_start),
+##	("255032", "",       db_end)
+##]
+'''
+iov_list = [
+# 2017 ###
+# 	("1", "299329",       db_import) # B
+# 	("297020", "299329",       db_import) # B
+# 	("299337", "302029",       db_import) # C
+# 	("302030", "303434",       db_import) # D
+# 	("302030", "304826",       db_import) # DE
+# 	("303435", "304826",       db_import) # E
+####	("304911", "306462",       db_import) # F
+ 	("304911", "",       db_import) # F
+# 
+# This is JEC internal definition, use this one instead of PPD definition
+# 2016 ####
+#	("1", "276811",       db_import) # BCD
+#	("276831",	"278801",       db_import) # EF
+#	("278802",	"280385",      db_import)  # G
+#	("280919",	"",      db_import) # H
+# 2016 V9, 10, 11 ####
+# 	("1", "276811",       db_import) # BCD
+#	("276831",	"278801",       db_import) # EF early F
+#	("278802",	"297019",      db_import)  # earlyF GH use this to combine with 2017
+####	("278802",	"284044",      db_import)  # earlyF GH
+]
+'''
+for aa in ALGO_LIST_AK4_base: #loop for jet algorithms
+    for iov in iov_list:
+        input_era = os.path.splitext(os.path.basename(iov[2]))[0]
+        s1 = jec_type + '_' + input_era + '_' + aa
+        s2 = jec_type + '_' + ERA + '_' + aa
+        if not iov[1]:
+            s = "conddb_import -f sqlite_file:"+iov[2]+" -c sqlite_file:"+ERA+".db -i "+s1+" -t "+s2+" -b "+iov[0]
+        else:
+            s = "conddb_import -f sqlite_file:"+iov[2]+" -c sqlite_file:"+ERA+".db -i "+s1+" -t "+s2+" -b "+iov[0]+" -e "+iov[1]
+        print s
+        subprocess.call(s.split())
+for aa in ALGO_LIST_AK4_ext: #loop for jet algorithms
+    for iov in iov_list:
+        input_era = os.path.splitext(os.path.basename(iov[2]))[0]
+        s1 = jec_type + '_' + input_era + '_' + aa
+        s2 = jec_type + '_' + ERA + '_' + aa
+        if not iov[1]:
+            s = "conddb_import -f sqlite_file:"+iov[2]+" -c sqlite_file:"+ERA+".db -i "+s1+" -t "+s2+" -b "+iov[0]
+        else:
+            s = "conddb_import -f sqlite_file:"+iov[2]+" -c sqlite_file:"+ERA+".db -i "+s1+" -t "+s2+" -b "+iov[0]+" -e "+iov[1]
+        print s
+        subprocess.call(s.split())
+for aa in ALGO_LIST_AK8_base: #loop for jet algorithms
+    for iov in iov_list:
+        input_era = os.path.splitext(os.path.basename(iov[2]))[0]
+        s1 = jec_type + '_' + input_era + '_' + aa
+        s2 = jec_type + '_' + ERA + '_' + aa
+        if not iov[1]:
+            s = "conddb_import -f sqlite_file:"+iov[2]+" -c sqlite_file:"+ERA+".db -i "+s1+" -t "+s2+" -b "+iov[0]
+        else:
+            s = "conddb_import -f sqlite_file:"+iov[2]+" -c sqlite_file:"+ERA+".db -i "+s1+" -t "+s2+" -b "+iov[0]+" -e "+iov[1]
+        print s
+        subprocess.call(s.split())
+
+
+
+
+
+
+
+
+'''
+1-252999 50ns
+253000-254832 25ns
+254833-254833 50ns
+254834-254980 25ns
+254981-255031 50ns
+255032-infinity 25ns
+
+conddb_import -f sqlite_file:Summer15_50nsV5_DATA.db -c sqlite_file:Summer15_V1_DATA.db -i JetCorrectorParametersCollection_Summer15_50nsV5_DATA_AK4PF -t JetCorrectorParametersCollection_Summer15_V1_DATA_AK4PF -b 1 -e 252999
+conddb_import -f sqlite_file:Summer15_25nsV2_DATA.db -c sqlite_file:Summer15_V1_DATA.db -i JetCorrectorParametersCollection_Summer15_25nsV2_DATA_AK4PF -t JetCorrectorParametersCollection_Summer15_V1_DATA_AK4PF -b 253000 -e 254832
+conddb_import -f sqlite_file:Summer15_50nsV5_DATA.db -c sqlite_file:Summer15_V1_DATA.db -i JetCorrectorParametersCollection_Summer15_50nsV5_DATA_AK4PF -t JetCorrectorParametersCollection_Summer15_V1_DATA_AK4PF -b 254833 -e 254833
+conddb_import -f sqlite_file:Summer15_25nsV2_DATA.db -c sqlite_file:Summer15_V1_DATA.db -i JetCorrectorParametersCollection_Summer15_25nsV2_DATA_AK4PF -t JetCorrectorParametersCollection_Summer15_V1_DATA_AK4PF -b 254834 -e 254980
+conddb_import -f sqlite_file:Summer15_50nsV5_DATA.db -c sqlite_file:Summer15_V1_DATA.db -i JetCorrectorParametersCollection_Summer15_50nsV5_DATA_AK4PF -t JetCorrectorParametersCollection_Summer15_V1_DATA_AK4PF -b 254981 -e 255031
+conddb_import -f sqlite_file:Summer15_25nsV2_DATA.db -c sqlite_file:Summer15_V1_DATA.db -i JetCorrectorParametersCollection_Summer15_25nsV2_DATA_AK4PF -t JetCorrectorParametersCollection_Summer15_V1_DATA_AK4PF -b 255032 -e infinity
+
+'''
---
+++ JEC/createInterleavedDatabaseUL17_18.sh
@@ -0,0 +1,36 @@
+
+
+
+
+
+#python createInterleavedDatabaseUL17_18.py --era Summer19UL17_V6_DATA  --importDB Summer19UL17_RunB_V6_DATA.db Summer19UL17_RunC_V6_DATA.db Summer19UL17_RunD_V6_DATA.db Summer19UL17_RunE_V6_DATA.db Summer19UL17_RunF_V6_DATA.db
+#python createInterleavedDatabaseUL17_18.py --era Summer19UL18_V5_DATA  --importDB Summer19UL18_RunA_V5_DATA.db Summer19UL18_RunB_V5_DATA.db Summer19UL18_RunC_V5_DATA.db Summer19UL18_RunD_V5_DATA.db
+
+
+### combine history of tags alt
+#for jet_type in AK4PF AK4PFchs AK4PFPuppi AK8PF AK8PFchs AK8PFPuppi
+#do
+#        conddb --yes copy --destdb offline_data_jec_alt.db  --to 315251 JetCorrectorParametersCollection_Sum16V11_and_UL17V5_and_Aut18V19_DATA_${jet_type} JetCorrectorParametersCollection_Run2_Combined_UL_2017_2018_DATA_${jet_type}
+#        conddb --yes copy --destdb offline_data_jec_alt.db --from 315252 --to 327564 JetCorrectorParametersCollection_Summer19UL18_V5_DATA_${jet_type} JetCorrectorParametersCollection_Run2_Combined_UL_2017_2018_DATA_${jet_type}
+#        conddb --yes copy --destdb offline_data_jec_alt.db  --from 327565 JetCorrectorParametersCollection_Sum16V11_and_UL17V5_and_Aut18V19_DATA_${jet_type} JetCorrectorParametersCollection_Run2_Combined_UL_2017_2018_DATA_${jet_type}
+#done
+#
+#
+#
+#
+### combine history of tags
+#for jet_type in AK4PF AK4PFchs AK4PFPuppi AK8PF AK8PFchs AK8PFPuppi
+#do
+#  conddb --yes copy --destdb offline_data_jec.db  --to 315251 JetCorrectorParametersCollection_Sum16V11_and_UL17V5_and_Aut18V19_DATA_${jet_type} JetCorrectorParametersCollection_Run2_Combined_UL_2017_2018_DATA_${jet_type} 
+#  conddb --yes copy --destdb offline_data_jec.db --from 315252 JetCorrectorParametersCollection_Summer19UL18_V5_DATA_${jet_type} JetCorrectorParametersCollection_Run2_Combined_UL_2017_2018_DATA_${jet_type}
+#done
+#
+##for sf_type in SF PtResolution PhiResolution EtaResolution
+##do
+##  for jet_type in AK4PFchs AK4PFPuppi AK8PFchs AK8PFPuppi
+##  do
+##      conddb --yes copy --destdb offline_data_jec.db  --to 307082 JR_dataRun2_RunABCD_25nsV1b_V3b_V7b_106X_DATA_${sf_type}_${jet_type} JR_dataRun2_Combined_UL_2017_2018_${sf_type}_${jet_type}
+##      conddb --yes copy --destdb offline_data_jec.db --from 307083 JR_Summer19UL18_JRV2_DATA_${sf_type}_${jet_type} JR_dataRun2_Combined_UL_2017_2018_${sf_type}_${jet_type}
+##  done
+##done
+#
